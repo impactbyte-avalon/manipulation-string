fe2977e4bd7d85d423d07b06ca76e3ef59df6912
thoriqnf <thoriqnfaizal@gmail.com>
2017-10-04 14:10:25 +0700

    level0-3

---
+++ index.js
@@ -0,0 +1,48 @@
+level 0
+const sentenceA = "Hello World!"
+const sentenceB = "Good Bye!"
+
+console.log(sentenceA)
+console.log(sentenceB)
+
+level 1
+const sentenceC = sentenceA + " " + sentenceB
+Hello World! Good Bye!
+const sentenceD = `${sentenceA} ${sentenceB}`
+Hello World! Good Bye!
+
+level 2
+Input example:
+
+```js
+"Hello World"
+"Good Bye"
+```
+
+Output example:
+
+```js
+"HELLO WORLD"
+"GOOD BYE"
+
+var sentenceA = "Hello World";
+var sentenceB = "Good Bye";
+
+var sentenceA2 = sentenceA.toUpperCase();
+var sentenceB2 = sentenceB.toUpperCase();
+
+level 3
+
+Input example:
+
+```js
+"hello goodness"
+```
+
+Output example:
+
+```js
+"Hello Goodness"
+
+var sentenceA3 = sentenceA.toTitleCase();
+var sentenceB3 = sentenceB.toTitleCase();
