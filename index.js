a804da6232b12e4c6e13d62fbc8a29adb6ad5d2b
Jessica Leaky <jessicaleaky@gmail.com>
2017-10-04 15:03:18 +0700

    Complete Level 1

--- index.js
+++ index.js
@@ -6,3 +6,12 @@ const sentenceB = "Good Bye!"
 
 console.log(sentenceA)
 console.log(sentenceB)
+
+
+// Level 1
+
+const sentenceC = sentenceA + " " + sentenceB
+const sentenceD = `${sentenceA} ${sentenceB}`
+
+console.log(sentenceC)
+console.log(sentenceD)
