c2c783a24243b4a3bfcdf05a69c91cadf0b7b7a9
thoriqnf <thoriqnfaizal@gmail.com>
2017-10-04 15:25:54 +0700

    benerin lowercase

--- index.js
+++ index.js
@@ -1,48 +1,22 @@
-level 0
+//declare some sentences
 const sentenceA = "Hello World!"
 const sentenceB = "Good Bye!"
 
+//declare some new sentences
+const sentenceC = sentenceA + " " + sentenceB
+
+const sentenceD = `${sentenceA} ${sentenceB}`
+
+//uppercase
+sentenceA.toUpperCase();
+
+sentenceB.toUpperCase();
+
+//titlecase
+sentenceA.toLowerCase();
+sentenceB.toLowerCase();
+
+//output
 console.log(sentenceA)
 console.log(sentenceB)
-
-level 1
-const sentenceC = sentenceA + " " + sentenceB
-Hello World! Good Bye!
-const sentenceD = `${sentenceA} ${sentenceB}`
-Hello World! Good Bye!
-
-level 2
-Input example:
-
-```js
-"Hello World"
-"Good Bye"
-```
-
-Output example:
-
-```js
-"HELLO WORLD"
-"GOOD BYE"
-
-var sentenceA = "Hello World";
-var sentenceB = "Good Bye";
-
-var sentenceA2 = sentenceA.toUpperCase();
-var sentenceB2 = sentenceB.toUpperCase();
-
-level 3
-
-Input example:
-
-```js
-"hello goodness"
-```
-
-Output example:
-
-```js
-"Hello Goodness"
-
-var sentenceA3 = sentenceA.toTitleCase();
-var sentenceB3 = sentenceB.toTitleCase();
+console.log(sentenceC)
