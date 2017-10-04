8f1a2dfd39e7b18fb5ddea86e7de99444273368f
Arian <arianmarkus77@yahoo.com>
2017-10-04 15:33:36 +0700

    Uppercase Lowercase

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const sentenceA = "Hello World!"
 const sentenceB = "Good Bye!"
+const sentenceC = sentenceA + " " + sentenceB
+const sentenceD = `${sentenceA} ${sentenceB}`
 
 console.log(sentenceA)
 console.log(sentenceB)
@@ -8,3 +10,17 @@ console.log(sentenceB)
  * Also a comment
  * with multiple lines
  */
+
+
+
+console.log(sentenceC)
+console.log(sentenceD)
+
+var string1 = "Hello World!";
+var result1 = string1.toUpperCase();
+
+var string2 = "Good Bye!";
+var result2 = string2.toUpperCase();
+
+console.log(result1)
+console.log(result2)
