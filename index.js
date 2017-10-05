c82ffbb30a70458c9948944807e74716ce72f459
rian <adrianpradana301@gmail.com>
2017-10-05 11:58:20 +0700

    new file for the assignment

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-	// This is assignment for level 0 and level 1
+//assignment for level 0 and level 1
 const name ="Adrian Pradana"
 const age  ="19 Tahun"
 console.log("Halo nama saya "+name+"Usia saya "+age)
@@ -8,7 +8,7 @@ console.log("Halo nama saya "+name+"Usia saya "+age)
 function capitalize(str) {
     var tempArray = str; //membuat string berspasi
     var newArray = [];//memasukkan array kapital
-  
+
     for (var i= 0; i < tempArray.length; i++) {
       newArray.push(tempArray[i].charAt(0).toUpperCase() + tempArray[i].slice(1));
     }
@@ -23,7 +23,7 @@ console.log(capitalize("lord megatron"));
 function title(str1) {
     var tempArray = str1.split(' '); //membuat string berspasi
     var newArray = [];//memasukkan array kapital
-  
+
     for (var i= 0; i < tempArray.length; i++) {
       newArray.push(tempArray[i].charAt(0).toUpperCase() + tempArray[i].slice(1));
     }
@@ -31,9 +31,3 @@ function title(str1) {
  }
 
 console.log(title("i am megatron"));
-
-
-
-
-
-
