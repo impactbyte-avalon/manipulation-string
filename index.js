eb0cb0475dff5a84a60c1ddd4a760d9be5041f54
Andrew <sbstn.andrew56@gmail.com>
2017-10-05 13:02:32 +0700

    tugas manip string baru lv 2

---
+++ index.js
@@ -0,0 +1,18 @@
+// helloworld
+// string
+const sentenceA = 'Hello World'
+const sentenceB = "Good Bye"
+
+// uppercase
+sentenceA.toUpperCase();
+sentenceB.toUpperCase(); 
+
+console.log(sentenceA);
+console.log (sentenceB);
+
+const sentenceC = sentenceA + " " + sentenceB
+const sentenceD = `${sentenceA} ${sentenceB}`
+
+console.log (sentenceC);
+console.log (sentenceD);
+
